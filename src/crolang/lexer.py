deba4ce6f977ffa1ed28a6b829eb1ee226773808
diodemusic <diodemusicuk@gmail.com>
2026-04-22 22:20:52 +0100

    Add Token subclasses

--- src/crolang/lexer.py
+++ src/crolang/lexer.py
@@ -7,3 +7,17 @@ from crolang.source import Span
 @dataclass(frozen=True)
 class Token(abc.ABC):
     span: Span
+
+
+@dataclass(frozen=True)
+class LeftBrace(Token): ...
+
+
+@dataclass(frozen=True)
+class IntLiteral(Token):
+    value: int
+
+
+@dataclass(frozen=True)
+class Identifier(Token):
+    name: str

6abca1fb9aaab199d4faf8178ac51a57456d25ac
diodemusic <diodemusicuk@gmail.com>
2026-04-22 22:33:33 +0100

    Implement Scanner class and lex function for token scanning

--- src/crolang/lexer.py
+++ src/crolang/lexer.py
@@ -1,12 +1,36 @@
 import abc
 from dataclasses import dataclass
 
-from crolang.source import Span
+from crolang.source import SourceFile, Span
 
 
 @dataclass(frozen=True)
 class Token(abc.ABC):
     span: Span
+
+
+class Scanner:
+    def __init__(self, source: SourceFile) -> None:
+        self.source: SourceFile = source
+        self.tokens: list[Token] = []
+        self.start: int = 0
+        self.current: int = 0
+
+    def at_end(self) -> bool: ...
+
+    def scan_token(self) -> None: ...
+
+    def scan_tokens(self) -> None:
+        while not self.at_end():
+            self.start = self.current
+            self.scan_token()
+
+
+def lex(source: SourceFile) -> list[Token]:
+    scanner: Scanner = Scanner(source=source)
+    scanner.scan_tokens()
+
+    return scanner.tokens
 
 
 @dataclass(frozen=True)

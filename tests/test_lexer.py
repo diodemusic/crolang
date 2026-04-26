05ae305be7f5aef577d6c4dea44ba85cdf8f347b
diodemusic <diodemusicuk@gmail.com>
2026-04-26 01:30:31 +0100

    Create int literal test

--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -1,9 +1,17 @@
+import pytest
+
 from crolang.lexer import scanner, token_classes
 from crolang.source import SourceFile
 
-tokens: list[token_classes.Token] = scanner.lex(
-    source=SourceFile(name="bird.cro", text="+ * % ^ - -> ? ?. < <= > >=")
-)
 
-for i in range(len(tokens)):
-    print(f"Token {i}: {tokens[i]}")
+@pytest.mark.parametrize(
+    argnames="test_input, expected", argvalues=[("42", 42), ("0", 0), ("12345", 12345)]
+)
+def test_int_literal(test_input: str, expected: int) -> None:
+    tokens: list[token_classes.Token] = scanner.lex(
+        source=SourceFile(name="bird.cro", text=test_input)
+    )
+
+    assert len(tokens) == 1
+    assert type(tokens[0]) is token_classes.IntLiteral
+    assert tokens[0].value == expected
